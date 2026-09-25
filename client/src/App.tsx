6d81893e395498dab43917524ab08845ceffe9f9
anagha0407 <anaghabalaji0407@gmail.com>
2026-09-25 23:03:41 +0530

    Fix problem page navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/login";
 import Register from "./pages/register";
-import Problems from "./pages/Problems";
+import ProblemsPage from "./pages/ProblemsPage";
 import ProblemDetailsPage from "./pages/ProblemDetailsPage.tsx";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
 
-                <Route path="/problems" element={<Problems />} />
+                <Route path="/problems" element={<ProblemsPage />} />
                 <Route path="/problems/:id" element={<ProblemDetailsPage />} />
 
                 <Route path="*" element={<Navigate to="/login" replace />} />
